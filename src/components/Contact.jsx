529868cf4453ace69c8c2f9a423936632a80a92a
Chandra Prakash <chandraprakashb15@gmail.com>
2021-12-01 14:37:22 +0530

    css fixes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,17 +13,23 @@ export default class Contact extends Component {
         </p>
         <p className="call-me">
           <img class="phone-icon" src="./images/telephone.svg"></img>
-          <h4>Call me :</h4>
-          <h6>+91-9014243818</h6>
+          <div class="contact-info">
+            <h5>Mobile:</h5>
+            <h6>+91-9014243818</h6>
+          </div>
         </p>
         <p className="email-me">
           <img class="mail-icon" src="./images/mail.svg"></img>
-          <h4>Email me :</h4>
-          <h6>chandraprakashb15@gmail.com</h6>
+          <div class="contact-info">
+            <h5>Email:</h5>
+            <h6>chandraprakashb15@gmail.com</h6>
+          </div>
         </p>
         <p className="follow-me">
-          <h4>Follow me :</h4>
-          <a href="https://www.linkedin.com/in/chandub15/"><img class="linkedin-icon" src="./images/linkedin.svg"></img> </a>
+        <a href="https://www.linkedin.com/in/chandub15/"><img class="linkedin-icon follow-icon" src="./images/linkedin.svg"></img> </a>
+          <div class="contact-info">
+            <h6>Follow me</h6>
+          </div>
         </p>
       </div>
     );
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,27 +1,24 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import "../scss/home.scss";
-import Contact from "./Contact";
-import { StaticImage } from "gatsby-plugin-image"
-import Events from "./About-Me";
 import HeroSection from "./hero-section";
 import AboutMe from "./About-Me";
 import Skills from "./Skills";
+import Contact from "./Contact";
 
 
 export default class Landing extends Component {
 
   componentDidMount(){
-    // document.addEventListener('contextmenu', event => event.preventDefault());
   }
   render() {
     return (
       <div  id="home">
         <Header></Header>
         <HeroSection></HeroSection>
-        {/* <AboutMe></AboutMe> */}
+        <AboutMe></AboutMe>
         <Skills></Skills>
-        {/* <Contact></Contact> */}
+        <Contact></Contact>
       </div>
     );
   }
