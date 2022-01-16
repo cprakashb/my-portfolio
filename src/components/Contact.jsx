c3fa390fb64f5de28d58fdf99c7f5b912722743f
cbommade <chandraprakashb15@gmail.com>
2022-01-17 02:17:10 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import "../scss/contact.scss";
 
 export default class Contact extends Component {
-  componentDidMount() {
-  }
+  componentDidMount() {}
   render() {
     return (
       <div className="contact-section" id="contact">
@@ -12,7 +11,11 @@ export default class Contact extends Component {
         </p>
         <p className="call-me">
           <a href="tel:+91-9014243818">
-            <img class="phone-icon" alt="phone-icon" src="./images/telephone.svg"></img>
+            <img
+              class="phone-icon"
+              alt="phone-icon"
+              src="./images/telephone.svg"
+            ></img>
             <div class="contact-info">
               <h5>Mobile:</h5>
               <h6>+91-9014243818</h6>
@@ -20,16 +23,26 @@ export default class Contact extends Component {
           </a>
         </p>
         <p className="email-me">
-        <a href="mailto:chandraprakashb15@gmail.com">
-          <img class="mail-icon" alt="mail-icon" src="./images/mail.svg"></img>
-          <div class="contact-info">
-            <h5>Email:</h5>
-            <h6>chandraprakashb15@gmail.com</h6>
-          </div>
+          <a href="mailto:chandraprakashb15@gmail.com">
+            <img
+              class="mail-icon"
+              alt="mail-icon"
+              src="./images/mail.svg"
+            ></img>
+            <div class="contact-info">
+              <h5>Email:</h5>
+              <h6>chandraprakashb15@gmail.com</h6>
+            </div>
           </a>
         </p>
         <p className="follow-me">
-          <a href="https://www.linkedin.com/in/chandub15/"><img class="linkedin-icon follow-icon" alt="linkedin-icon" src="./images/linkedin.svg"></img> </a>
+          <a href="https://www.linkedin.com/in/chandub15/">
+            <img
+              class="linkedin-icon follow-icon"
+              alt="linkedin-icon"
+              src="./images/linkedin.svg"
+            ></img>{" "}
+          </a>
           <div class="contact-info">
             <h5>Follow me</h5>
           </div>
