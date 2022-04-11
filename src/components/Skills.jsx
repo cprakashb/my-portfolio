dae31374119fc418e67fd77c39329ec86ebe9659
cbommade <chandraprakashb15@gmail.com>
2022-04-11 15:44:08 +0530

    icon colors updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,19 +6,19 @@ export default class Skills extends Component {
   state = {
     skills: {
       'Web Development': {
-        'icon': './images/webdev.svg', 'technologies': ['Java Script', 'HTML5', 'CSS'
+        'icon': './images/webdev_white.svg', 'technologies': ['Java Script', 'HTML5', 'CSS'
           , 'Node JS ', 'React', 'TypeScript']
       },
       'Frameworks': {
-        'icon': './images/database.svg', 'technologies': [
+        'icon': './images/framework_white.svg', 'technologies': [
           'Next.js ', 'Gatsby ', 'Angular ', 'Express', 'Strapi ', 'Material ',
           'Bootstrap', 'Spring Boot (Basics)', 'Jest ', 'Storybook']
       },
-      'Database': { 'icon': './images/framework.svg', 'technologies': ['MongoDB ', 'MySQL ', 'Cloudant', 'CouchDB'] },
+      'Database': { 'icon': './images/database_white.svg', 'technologies': ['MongoDB ', 'MySQL ', 'Cloudant', 'CouchDB'] },
       'Devops': { 'icon': './images/process.png', 'technologies': ['AWS', 'Jenkins'] },
       'IDE and other tools': { 'icon': './images/tools_white.svg', 'technologies': ['Visual Studio', 'Postman'] },
       'Others': {
-        'icon': './images/others.svg', 'technologies': ['Vercel ', ' GitHub ', ' Docker', ' Microservices Architecture ',
+        'icon': './images/others_white.svg', 'technologies': ['Vercel ', ' GitHub ', ' Docker', ' Microservices Architecture ',
           ' SQL ', ' NOSQL ', ' Agile Methodology ', ' CI/CD ', ' JIRA']
       }
 
