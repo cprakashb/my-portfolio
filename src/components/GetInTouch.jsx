af457cee44bd80276da3a5219f08d11d968f3cae
chandub15 <chandraprakashb15@gmail.com>
2022-10-14 23:28:47 -0400

    Update GetInTouch.jsx

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -11,7 +11,7 @@ export default class GetInTouch extends Component {
         <h1>Get In Touch</h1>
         <p className="line-1">Want to know how I may help you? or you have a question or just want to say hi,
            My inbox is always open. I'll try my best to get back to you!</p>
-        <a className="say-hello" href="mailto:chandraprakashb15@gmail.com" target="_blank" rel="noreferrer">Say hello</a>
+        <a className="say-hello" href="mailto:cprakashb.dev@gmail.com" target="_blank" rel="noreferrer">Say hello</a>
       </div>
     );
   }
