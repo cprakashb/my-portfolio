216790aa6a88d7255c9e96a1341169da25e423b4
Chandra Prakash <chandraprakashb15@gmail.com>
2023-06-17 20:55:46 -0400

    created build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,6 @@
 import * as React from "react"
 import "@popperjs/core/dist/umd/popper.min"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min"
 import Landing from "../components/Landing"
-
-
 
 const IndexPage = () => {
   return <div className="container container-fluid"><Landing></Landing></div>
