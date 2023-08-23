106d8d0966507b7bc9ac0bd7e6022a1b262efcbf
Chandra Prakash <chandraprakashb15@gmail.com>
2023-08-23 00:30:35 -0400

    removed div

--- src/components/Work-History.jsx
+++ src/components/Work-History.jsx
@@ -41,13 +41,12 @@ export default class WorkHistory extends Component {
                 <p class="lead text-muted pb-3">Jan 2021 - August 2022</p>
               </div>
             </div>
-          </div>
 
 
           
             <div class="d-flex mb-1 gap-3">
               <div class="d-flex flex-column pr-4 align-items-center">
-                <div class="rounded-circle py-2 px-3 stepper-number text-white mb-1">1</div>
+                <div class="rounded-circle py-2 px-3 stepper-number text-white mb-1">3</div>
                 <div class="line h-100"></div>
               </div>
               <div>
@@ -59,7 +58,6 @@ export default class WorkHistory extends Component {
 
           
 
-          
        
         </div>
 
